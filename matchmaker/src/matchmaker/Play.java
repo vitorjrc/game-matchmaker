b622063808b217147ceb32274e7440d0337d9ad8
theManInSpace <v.j.r.c1997@hotmail.com>
2017-12-04 23:58:29 +0000

    Added Play class
    It supports already 2 teams. Every User has a Champion.

---
+++ matchmaker/src/matchmaker/Play.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matchmaker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ *
+ * @author Vitor Castro
+ */
+public class Play {
+    
+    // O arraylist guarda o user e o boneco que o mesmo escolheu
+    private Map<User,String> team1 = null;
+    private Map<User,String> team2 = null;
+    private int ranking;
+    
+    public Play(int rank) {
+        team1 = new HashMap<>();
+        team2 = new HashMap<>();
+        ranking = rank;
+    }
+    
+}
