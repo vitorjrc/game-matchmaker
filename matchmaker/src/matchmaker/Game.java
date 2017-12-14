97e617ca832190d4fc3565808faa2fd7b7d8b683
theManInSpace <v.j.r.c1997@hotmail.com>
2017-12-14 15:06:09 +0000

    Ended Game class with ConcurrentHashMap
    After having all done we gotta synchronize all the methods and remove the "Concurrent" of the HashMap, due to teacher's requisits. But for now we can develop like that to be sure that no errors happen here.

--- matchmaker/src/matchmaker/Game.java
+++ matchmaker/src/matchmaker/Game.java
@@ -5,23 +5,60 @@
  */
 package matchmaker;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *
  * @author Vitor Castro
  */
+// POR ENQUANTO ESTÁ COM CONCURRENT HASHMAP MAS DEPOIS TEM QUE SE IMPLEMENTAR MÉTODOS SYNCHRONIZED
 public class Game {
 
-    private static HashMap<Integer, Play> games = null;
+    // tem os jogos atualmente ativos, por ranking. faz-se remove quando é finalizado
+    private static ConcurrentHashMap<Integer, Play> plays = null;
 
     public Game() {
 
-        games = new HashMap<Integer, Play>(); // Integer é o rank do jogo
+        plays = new ConcurrentHashMap<Integer, Play>(); // Integer é o rank do jogo
     }
 
-    public boolean isPlayStarted(int rank) {
+    /*
+    Retorna a play com aquele rank
+     */
+    public Play getPlay(int rank) {
 
-        // meter a consultar map
+        return plays.get(rank); // não fazemos clone porque queremos mesmo modificar este objeto no ServerWorker, e não uma cópia
+    }
+
+    /*
+    Devolve o rank da play em que o jogador pode entrar. -1 caso seja necessário criar um novo jogo
+     */
+    public int isPlayStarted(int rank) {
+
+        if (plays.containsKey(rank)) {
+            return rank;
+        } else if (plays.containsKey(rank - 1)) {
+            return rank - 1;
+        } else if (plays.containsKey(rank + 1)) {
+            return rank + 1;
+        } else {
+            return (-1);
+        }
+    }
+
+    /*
+    Insere nova play no jogo
+     */
+    public void launchNewPlay(int rank, Play newPlay) {
+
+        plays.put(rank, newPlay);
+    }
+
+    /*
+    Remove play finalizada do jogo
+     */
+    public void endPlay(int rank) {
+
+        plays.remove(rank);
     }
 }
